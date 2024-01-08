481ca92842870b083f33ccfab920bd91e50044db
iveth-cocha <maria.iveth16@outlook.es>
2024-01-08 16:42:12 -0500

    verificacion de registro

--- src/controllers/veterinario_controller.js
+++ src/controllers/veterinario_controller.js
@@ -1,11 +1,21 @@
+import Veterinario from "../models/Veterinario.js"
+
 const login =(req,res)=>{
     res.status(200).json({res:'login del veterinario'})
 }
 const perfil=(req,res)=>{
     res.status(200).json({res:'perfil del veterinario'})
 }
-const registro =(req,res)=>{
-    res.status(200).json({res:'registro de un nuevo veterinario'})
+const registro = async (req,res)=>{
+    const {email,password} = req.body
+    if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+    const verificarEmailBDD = await Veterinario.findOne({email})
+    if(verificarEmailBDD) return res.status(400).json({msg:"Lo sentimos, el email ya se encuentra registrado"})
+    const nuevoVeterinario = new Veterinario(req.body)
+    nuevoVeterinario.password = await nuevoVeterinario.encrypPassword(password)
+    nuevoVeterinario.crearToken()
+    await nuevoVeterinario.save()
+    res.status(200).json({nuevoVeterinario})
 }
 const confirmEmail = (req,res)=>{
     res.status(200).json({res:'confirmar email de registro de veterinario'})
