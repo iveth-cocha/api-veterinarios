cf1c4f2ffb8e270101044eb8a1d6ad8131f0d0b2
iveth-cocha <maria.iveth16@outlook.es>
2024-01-10 17:18:34 -0500

    registro en base

--- src/server.js
+++ src/server.js
@@ -23,13 +23,13 @@ app.use(express.json())
 
 
 // *-*-*-*-Rutas 
-// app.get('/',(req,res)=>{
-//     res.send("Server on")
+//app.get('/',(req,res)=>{
+//    res.send("Server on")
 // })
 
-app.use('/api',routerVeterinarios)
+ app.use('/api',routerVeterinarios)
 // Manejo de una ruta que no sea encontrada
-app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
+ app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
 
 
 //-*-*-*-*-*-exportar la variable
